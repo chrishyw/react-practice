85f9fd1b1b69c78316ef148508ad4f59eeda5e88
Christopher Wang <hycwang@ucdavis.edu>
2018-12-17 00:50:09 -0800

    added console.log

--- src/App.js
+++ src/App.js
@@ -191,6 +191,7 @@ class App extends Component {
   }
 
   formSubmit = (e) => {
+    console.log("test");
     e.preventDefault();
     this.setState({searchResult: this.searchResult});
     this.searchResult.trim().length > 1 ? alert("form submitted!") : alert("empty input or not long enough!");
